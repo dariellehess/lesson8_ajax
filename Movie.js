277f7cebb20c57f1e997e21cc3072d5b1b17e6d2
Darielle Hess <dsh16c@my.fsu.edu>
2018-08-02 01:39:00 +0000

    lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=b8d4dda70b2ade229776d6b08e69cb12',
                 data: query
             })
             
@@ -57,7 +57,12 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
+               
+
+                //The following code retrieves the movie title from JSON object retrieved from the TMDB Server
+                //and places the title on the HTML page, where the id="overview".
                 
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -72,7 +77,14 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/               
 
+               
+                
 
+                //The following code retrieves the movie title from JSON object retrieved from the TMDB Server
+                //and places the title on the HTML page, where the id="release_date".
+                
+                $("#release_date").html(json.results[0].release_date);
+                
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -86,7 +98,14 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/   
+                    
+                
 
+                //The following code retrieves the movie title from JSON object retrieved from the TMDB Server
+                //and places the title on the HTML page, where the id="popularity".
+                
+                $("#popularity").html(json.results[0].popularity);
+                
   
 
                 
@@ -101,7 +120,12 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/                 
+                
 
+                //The following code retrieves the movie title from JSON object retrieved from the TMDB Server
+                //and places the title on the HTML page, where the id="orignial_language".
+                
+                $("#original_language").html(json.results[0].original_language);
 
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+8/1/18
 
 ### Student Name:  
-(First and Last Name)
+Darielle Hess
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+I made a website that allows you to look up the movie title, plot, release date, popularity, language, and poster of any movie.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://dariellehess.github.io/lesson8_ajax/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. I learned that JSON is JavaScript Object Notation, it is a syntax for storing and exchanging data, and it is an easy language for humans to learn.
+2. I learned that a synchronous request stops the processing of the JavaScript code until a response is returned from the server.
+3. I learned that AJAX relies on JavaScript and HTTP requests to exchange data.
